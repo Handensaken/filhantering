3e354e4d8de5a30d071f8499705823a4f73482b4
Handensaken <Handensaken@users.noreply.github.com>
2020-09-15 12:20:29 +0200

    initial commit

---
+++ Filhantering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Filhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
